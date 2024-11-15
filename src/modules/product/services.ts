b1fa7edfd67ee36887feb9ccc909dafc2a4cd828
trieuquocbui <buiquoctrieu020402@gmail.com>
2024-11-15 16:31:45 +0700

    update

--- src/modules/product/services.ts
+++ src/modules/product/services.ts
@@ -1,7 +1,8 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, Product } from '@prisma/client'
 import { CreateProductDto, UpdateProductDto, ProductQuery } from './dto'
 import { prisma } from '../.././prismaClient'
 import { createImage } from '../../config/cloudinaryConfig'
+import { NextFunction } from 'express'
 
 export class ProductService {
   async createProduct(dto: CreateProductDto) {
@@ -32,13 +33,31 @@ export class ProductService {
     })
   }
 
-  async getProductById(productId: string) {
-    return await prisma.product.findUnique({
-      where: { productId },
-      include: {
-        category: true
-      }
-    })
+  async getProductById(productId: string, next: NextFunction): Promise<Product | undefined> {
+    try {
+      let product = await prisma.product.findUnique({
+        where: { productId }
+      })
+      return product
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getRandProducts(next: NextFunction): Promise<Product[] | undefined> {
+    try {
+      const products = await prisma.product.findMany({
+        take: 10,
+        orderBy: {
+          productId: 'asc'
+        }
+      })
+      const shuffled = products.sort(() => 0.5 - Math.random())
+      return shuffled.slice(0, 10)
+      return products
+    } catch (error) {
+      next(error)
+    }
   }
 
   async updateProduct(productId: string, dto: UpdateProductDto) {
@@ -56,5 +75,21 @@ export class ProductService {
       where: { productId },
       data: { isActive: false }
     })
+  }
+
+  async getProductByCategoryById(categoryId: string, next: NextFunction): Promise<Product[] | undefined> {
+    try {
+      let products = []
+      if (categoryId == 'all') {
+        products = await prisma.product.findMany()
+      } else {
+        products = await prisma.product.findMany({
+          where: { categoryId: categoryId }
+        })
+      }
+      return products
+    } catch (error) {
+      next(error)
+    }
   }
 }
